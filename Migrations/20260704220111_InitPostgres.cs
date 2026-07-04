8105e58ea98002327fdd94c7e01def409e090a3f
ArtMoon007 <artem12375po@gmail.com>
2026-07-05 01:02:43 +0300

    Switch to PostgreSQL

---
+++ Migrations/20260704220111_InitPostgres.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneratorGame.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Generator",
+                table: "Scores",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldDefaultValue: "bitebynight");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Generator",
+                table: "Scores",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "bitebynight",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
