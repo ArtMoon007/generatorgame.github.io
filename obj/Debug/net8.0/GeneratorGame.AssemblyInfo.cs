7f31507bd5be0de698ddc1d40da372b47c5816d0
ArtMoon007 <artem12375po@gmail.com>
2026-07-09 11:33:32 +0300

    222324

--- obj/Debug/net8.0/GeneratorGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/GeneratorGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeneratorGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a130d205251ce9f4f21b60ec4a6937792db11871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2afe99dc0852f8640206c40642f80d869342ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeneratorGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeneratorGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
